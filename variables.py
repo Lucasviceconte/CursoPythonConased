2e63c0af1bdb46beab661af55f06d606594aadde
Lucasviceconte <lucasviceconte92@gmail.com>
2025-05-28 18:02:06 -0300

    Clase 1 - Variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+#Num. Enteros -> num1 = 10
+#Num. Flotantes -> flo1 = 10.5
+#String -> cadeba = "HOla, Python!"
+#Booleanos -> booleano = True
+
+numero = int(input("Ingrese un numero: ") )
+print(type(numero))
